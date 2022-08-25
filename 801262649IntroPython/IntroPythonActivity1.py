f8cd94da87f7dd0c2573f282c9e4175ee571e252
JeremieKoehler <jeremiekoehler@live.com>
2022-08-24 17:09:03 -0700

    First Commit

---
+++ 801262649IntroPython/IntroPythonActivity1.py
@@ -0,0 +1,9 @@
+#program 1
+def stringBothEnds(str):
+    if str.length < 2
+        return "Your String is too short! /n"
+    return str[0:2] + str[-2:]  
+
+string1 = input("Please enter a string: /n")
+string2 = stringBothEnds(string1)
+print (string2)
---
+++ 801262649IntroPython/IntroPythonActivity2.py
@@ -0,0 +1,8 @@
+#program 2
+num1 = input("Please enter a number: /n")
+num2 = input("Please enter a second number: /n")
+str1 = ("")
+for x in range(num1, num2):
+    if (x % 7) == 0
+    str1 = str1 + x + ", "
+print (str1)
